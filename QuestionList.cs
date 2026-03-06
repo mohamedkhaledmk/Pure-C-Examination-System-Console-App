9f798d0d624eea8526c999e65550a4084822b57c
mohamedkhaledmk <khaled25256@gmail.com>
2026-03-06 17:44:31 +0200

    FEAT: QuestionList Class Added

--- QuestionList.cs
+++ QuestionList.cs
@@ -6,9 +6,19 @@ namespace ConsoleApp1
 {
     internal class QuestionList:List<Question>
     {
-        public new virtual void Add(Question q)
+        public readonly string FileName;
+        public QuestionList(string fn)
+        {
+            FileName = fn;
+        }
+        //i think it it would be better if i made add new virtual function
+        public new void Add(Question q)
         {
             base.Add(q);
+            using (StreamWriter sw = new StreamWriter(FileName,true))
+            {
+                sw.WriteLine(q);
+            };
         }
     }
 }
