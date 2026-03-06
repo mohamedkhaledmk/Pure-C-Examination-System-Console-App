94712a4bde1d08c11b8995ed8f5a57a405d5df29
mohamedkhaledmk <khaled25256@gmail.com>
2026-03-06 20:14:25 +0200

    FEAT: exameventargs file

---
+++ ExamEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+
+    internal class ExamEventArgs:EventArgs
+    {
+        public Subject Subject { get; }    
+        public Exam Exam { get; }
+    }
+}
