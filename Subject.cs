7f7e77acc492a22cd8c79f14b67052be7d2c9265
mohamedkhaledmk <khaled25256@gmail.com>
2026-03-06 20:08:32 +0200

    Initial Version of subject class

---
+++ Subject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Subject
+    {
+        public string Name { get; set; }
+        public List<Student> EnrolledStudents;
+
+        //Methods
+        public bool Enroll(Student stud)
+        {
+            if (!EnrolledStudents.Contains(stud))
+            {
+                EnrolledStudents.Add(stud);
+                return true;
+            }
+                return false;
+        }
+
+    }
+}
